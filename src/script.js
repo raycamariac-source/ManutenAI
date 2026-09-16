e2195d6053e5c19763204d4727d000092a983c19
raycamariac-source <raycamariac@gmail.com>
2026-09-16 09:41:29 -0300

    Implementar registro de problemas

--- src/script.js
+++ src/script.js
@@ -1,1 +1,24 @@
+const form = document.getElementById("formProblema");
+const listaProblemas = document.getElementById("listaProblemas");
 
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const local = document.getElementById("local").value;
+    const tipo = document.getElementById("tipo").value;
+    const descricao = document.getElementById("descricao").value;
+
+    const novaLinha = document.createElement("tr");
+
+    novaLinha.innerHTML = `
+        <td>${tipo}</td>
+        <td>${local}</td>
+        <td>Pendente</td>
+    `;
+
+    listaProblemas.appendChild(novaLinha);
+
+    form.reset();
+
+    alert("Problema registrado com sucesso!");
+});
