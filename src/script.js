4b98c685d9504a2620982dd3784dc467b59521d6
raycamariac-source <raycamariac@gmail.com>
2026-09-16 10:13:51 -0300

    Corrigir atualização de status dos problemas

--- src/script.js
+++ src/script.js
@@ -29,6 +29,7 @@ form.addEventListener("submit", function(event) {
         const status = novaLinha.querySelector(".status");
 
         status.textContent = "Resolvido";
+
         botaoResolver.remove();
     });
 
